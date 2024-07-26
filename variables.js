51872d8b0e0bd03f503e6472d8b022914ec2e82f
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-07-26 07:53:10 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,18 @@
+//  console.log("yes");
+
+ //var k sath yeh chij possible h 
+ var name="Keanu Reeves";
+ var name= "John Cena";
+ console.log(name);
+ //no error output will be john cena
+
+
+ //but you cannot do same with let
+
+ let name1 ="Keanu Reeves";
+ let name1 ="John Cena";
+ //as obvious it is showing error
+
+ const x=1;
+ x='akash';
+ console.log(x);
