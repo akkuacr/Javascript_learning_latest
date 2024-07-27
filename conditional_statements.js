a60d0b2777c9d5108f116bafb8e221543f79ee66
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-07-27 08:12:24 +0530

    Create conditional_statements.js

---
+++ conditional_statements.js
@@ -0,0 +1,32 @@
+ let age =18;
+ if(age>=18)
+ {
+    console.log("you are eligible");
+ }else{
+    console.log("you are not eligible");
+ }
+
+
+ if(age<15)
+ {
+    console.log("virgin mijito");
+ }else if(age>15 && age<18)
+ {
+    console.log("Fuck off buddy");
+ }else{
+    console.log("Go fuck off");
+ }
+
+let ch=1;
+ switch (ch){
+    case 1:
+        console.log("I am "+ch);
+        break;
+    case 2:
+        console.log("I am "+ ch);
+        break;
+     default:
+        break;    
+ }
+
+
