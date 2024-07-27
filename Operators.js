70d277a52c264be4882fce0d032105fc0ea2d25e
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-07-27 08:00:43 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,45 @@
+// Operators 
+// Airthmetic Operator
+ console.log(10*5);
+ console.log(10+5);
+ console.log(10-5);
+ console.log(10%5);
+ //This is the power operator
+ console.log(10**5);
+
+ //increment operator
+
+ let a=1;
+ a++;
+ console.log(a);
+
+
+ //most interesting Comparisons operator
+ a=5;
+ b=6;
+ console.log(a==b);
+ console.log(a!=b);
+ console.log(a<b);
+ console.log(a>=b);
+ console.log(a<=b);
+
+
+ a="3";
+ b=3;
+
+ console.log(typeof a);
+
+ //it will first perform type conversion then compare
+ console.log(a==b);
+
+ //it will consider data types also 
+ console.log(b===a);
+
+
+ a=true;
+ b=false;
+
+ console.log(a||b);
+ console.log(a&&b);
+console.log(!a);
+console.log(!b);
