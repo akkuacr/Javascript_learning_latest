f48e91393d8b3bc60c4fb84caf62f36ebfbb4650
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-08-19 20:34:31 +0530

    Update for_in_for_of.js

--- for_in_for_of.js
+++ for_in_for_of.js
@@ -1,9 +1,12 @@
  let arr=[10,20,30,40]
+
+//issse woh har index m jaega 
  for(let ele in arr)
  {
    console.log(ele);
  }
 
+//isse woh har element m hop krega
  for(let ar of arr)
  {
    console.log(ar);
