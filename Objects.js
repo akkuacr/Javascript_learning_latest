a14bdcd0ec40b48ad4db2f5b37c49cf0901cdcf3
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-08-21 07:12:49 +0530

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -31,6 +31,45 @@ person.fullName();
 
 
 
+ let obj ={
+    name:'Bravo',
+    age:21,
+    'last_name':'Dj',
+    func:function (){
+        console.log("I am function");
+    }
+    ,
+    arr:[20,30,40,50 ,60]
+ }
+
+
+console.log(obj['name']);
+console.log(obj.name);
+console.log(obj.last_name);
+console.log(obj.arr);
+
+//if you want to enter new key value pair
+obj.key='item1';
+console.log(obj);
+
+//similar thing you can do as 
+let a ='key2';
+obj[a]='value2';
+console.log(obj);
+
+//loop in object
+
+for(let i in obj){
+    //i m key hogi
+    console.log(`keys are ${i} and values are ${obj[i]}`);
+}
+//similar things you can do with of method als
+
+for( let  i of Object.keys(obj)){
+    console.log(`keys are ${i} and values are ${obj[i]}`);
+}
+
+
 
 
 
