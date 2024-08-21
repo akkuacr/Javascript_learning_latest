9a64399d1a801448ea4bc71d8b4f5f287c19829b
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-08-21 08:38:34 +0530

    Create HigherOrderFunctions.js

---
+++ HigherOrderFunctions.js
@@ -0,0 +1,86 @@
+ //function inside function
+
+ function fun(){
+    console.log("i am function 1");
+      
+    //fun2 ka scope kewal fun k andar tak hi h
+
+    function fun2() {
+        console.log("I am function 2");
+    }
+    fun2()
+
+
+ }
+ //fun ka global scope h
+
+ fun();
+
+
+ //high order functions in javascript
+
+ const powerTwo = (n)=>{
+    return n ** 2;
+ }
+
+ //function is passes as argument
+ function powerCube(powerTwo,n){
+    return powerTwo(n)*n;
+
+
+ }
+//here we are using function as a variable
+ console.log(powerCube(powerTwo,10));
+
+
+
+
+ //function can be return as a .result of another function
+
+ function sayHello(){
+    return ()=>{
+        console.log("Hello World");
+    }
+ }
+
+ const guessValue = sayHello();
+
+ guessValue()
+
+
+ const higherOrder = (n)=>{
+    const onefun = m =>{
+        const twofun = p=>{
+            return m+n+p;
+        }
+        return twofun
+    }
+    return onefun
+
+
+}
+
+//to run this kind of function 
+//syntax would be
+console.log(higherOrder(9)(10)(11));
+
+
+const sumArray = arr=>{
+    let total=0;
+    arr.forEach(element => {
+        total+=element;
+    });
+
+    return total;
+}
+
+console.log(sumArray([1,2,3,4,5,6,7]));
+
+function oneMoreHello(){
+    console.log("Hello Akash");
+}
+
+//similar type use hota h 
+//setInterval(oneMoreHello,1000);
+setTimeout(oneMoreHello,2000);
+
